6758a3410b0b95de34a25d1106c3cb483f89be3a
Ebuka Chigozie <kingieme@yahoo.com>
2025-09-22 14:21:06 +0100

    Fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -85,7 +85,7 @@ export default function RootLayout({
             )}
           </header>
           {/* Render page content */}
-          <main className="bg-blue-700 w-full block">{children}</main>
+          <main className="mainClass">{children}</main>
           <Toaster richColors position="top-center" /> {/* ← toast container */}
         </UserProvider>
       </body>
